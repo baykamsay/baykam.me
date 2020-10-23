f8ed8dde8aaeba080b8c890b19415e45cd09b012
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2020-10-23 10:58:56 +0300

    Removed bootstrap

--- components/navigation.js
+++ components/navigation.js
@@ -1,32 +1,32 @@
-import Link from "next/link";
-import { Navbar, Nav, Container } from "react-bootstrap";
+// import Link from "next/link";
+// import { Navbar, Nav, Container } from "react-bootstrap";
 
-export default function Navigation() {
-  return (
-    <Navbar bg="white" expand="md" fixed="top" className="shadow-sm">
-      {/* remove shadow from home maybe */}
-      <Container>
-        <Link href="/" passHref>
-          <Navbar.Brand>Home</Navbar.Brand>
-        </Link>
-        <Navbar.Toggle aria-controls="navbar" />
-        <Navbar.Collapse id="navbar">
-          <Nav className="ml-auto">
-            <Link href="/about" passHref>
-              <Nav.Link>About</Nav.Link>
-            </Link>
-            <Link href="/projects" passHref>
-              <Nav.Link>Projects</Nav.Link>
-            </Link>
-            <Link href="/blog" passHref>
-              <Nav.Link>Blog</Nav.Link>
-            </Link>
-            <Link href="/contact" passHref>
-              <Nav.Link>Contact</Nav.Link>
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-  );
-}
+// export default function Navigation() {
+//   return (
+//     <Navbar bg="white" expand="md" fixed="top" className="shadow-sm">
+//       {/* remove shadow from home maybe */}
+//       <Container>
+//         <Link href="/" passHref>
+//           <Navbar.Brand>Home</Navbar.Brand>
+//         </Link>
+//         <Navbar.Toggle aria-controls="navbar" />
+//         <Navbar.Collapse id="navbar">
+//           <Nav className="ml-auto">
+//             <Link href="/about" passHref>
+//               <Nav.Link>About</Nav.Link>
+//             </Link>
+//             <Link href="/projects" passHref>
+//               <Nav.Link>Projects</Nav.Link>
+//             </Link>
+//             <Link href="/blog" passHref>
+//               <Nav.Link>Blog</Nav.Link>
+//             </Link>
+//             <Link href="/contact" passHref>
+//               <Nav.Link>Contact</Nav.Link>
+//             </Link>
+//           </Nav>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import Head from "next/head";
-import { Container, Row, Col } from "react-bootstrap";
-import styles from "../styles/Home.module.scss";
+// import styles from "../styles/Home.module.scss";
 // import Link from "next/link";
 
 export default function Home() {
@@ -12,71 +11,57 @@ export default function Home() {
       </Head>
 
       <main>
-        <Container style={{ maxWidth: 800 }}>
-          <Row
-            className={styles.row}
-            style={{ minHeight: "100vh", padding: "56px 0" }}
-          >
-            <Col
-              style={{
-                // backgroundColor: "red",
-                justifyContent: "center",
-                display: "flex",
-                flexFlow: "column",
-              }}
+        <div className="container">
+          <h1 style={{ fontSize: "3rem" }}>Hello, I'm Baykam.</h1>
+          <p>
+            Currently, I am studying computer engineering at{" "}
+            <a
+              href="https://w3.bilkent.edu.tr/bilkent/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="bilkent"
             >
-              <h1 style={{ fontSize: "3rem" }}>Hello, I'm Baykam.</h1>
-              <p>
-                Currently, I am studying computer engineering at{" "}
-                <a
-                  href="https://w3.bilkent.edu.tr/bilkent/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="bilkent"
-                >
-                  Bilkent University
-                </a>
-                .
-              </p>
-              <p>
-                You can follow my work and look at my projects on my{" "}
-                <a
-                  href="https://github.com/baykamsay"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="github"
-                >
-                  GitHub
-                </a>{" "}
-                profile. I am currently open to working opportunities. If you'd
-                like, you can view my{" "}
-                <a
-                  href="https://www.linkedin.com/in/baykam-say/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="linkedin"
-                >
-                  LinkedIn
-                </a>{" "}
-                profile or download my{" "}
-                <a href="/baykam-say-cv.pdf" className="resume" download>
-                  resume
-                </a>
-                . If you are just here to get to know me or have a chat, you can
-                follow me on{" "}
-                <a
-                  href="https://twitter.com/BaykamSay"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="twitter"
-                >
-                  Twitter
-                </a>
-                .
-              </p>
-            </Col>
-          </Row>
-        </Container>
+              Bilkent University
+            </a>
+            .
+          </p>
+          <p>
+            You can follow my work and look at my projects on my{" "}
+            <a
+              href="https://github.com/baykamsay"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="github"
+            >
+              GitHub
+            </a>{" "}
+            profile. I am currently open to working opportunities. If you'd
+            like, you can view my{" "}
+            <a
+              href="https://www.linkedin.com/in/baykam-say/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="linkedin"
+            >
+              LinkedIn
+            </a>{" "}
+            profile or download my{" "}
+            <a href="/baykam-say-cv.pdf" className="resume" download>
+              resume
+            </a>
+            . If you are just here to get to know me or have a chat, you can
+            follow me on{" "}
+            <a
+              href="https://twitter.com/BaykamSay"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="twitter"
+            >
+              Twitter
+            </a>
+            .
+          </p>
+        </div>
       </main>
     </div>
   );
